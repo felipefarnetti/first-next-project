6517af7d6be42f28230ead44cdf04f2a6ea2258e
Felipe Farnetti <felipefarnetti@MacBook-Air-de-Felipe.local>
2023-09-06 17:41:33 +0200

    first commit

--- pages/add.js
+++ pages/add.js
@@ -10,7 +10,9 @@ export default function Add() {
       en: enWord.current.value,
       fr: frWord.current.value,
     };
-    fetch("https://" + process.env.VERCEL_URL + "/api/vocapi", {
+    const deploymentURL = window.location.origin; // Get the base URL of the current page
+    fetch(`${deploymentURL}/api/vocapi`, {
+      // Construct the full URL
       method: "POST",
       body: JSON.stringify(newWord),
       headers: {
